7b7447c8ca5eb5d40e793114a82c99826fbfb91f
Xiaodong Chen <chxd99@gmail.com>
2014-05-30 00:41:01 +0800

    got the last deal

--- start.py
+++ start.py
@@ -1,16 +1,23 @@
 from urllib2 import Request, urlopen
-req = Request('http://t.btc123.com/m.js?type=bitstampTicker')
-try:
-    response = urlopen(req)
-    html = response.read()
-    print html
-except URLError as e:
-    if hasattr(e, 'reason'):
-        print 'We failed to reach a server.'
-        print 'Reason: ', e.reason
-    elif hasattr(e, 'code'):
-        print 'The server couldn\'t fulfill the request.'
-        print 'Error code: ', e.code
-else:
-    # everything is fine
-    print "\n"
+import json
+baseurl = 'http://t.btc123.com/m.js'
+def get_full_json(baseurl, ticker):
+    req = Request(baseurl + '?type=' + ticker)
+    try:
+        response = urlopen(req)
+        json_str = response.read()
+        return json_str 
+    except URLError as e:
+        if hasattr(e, 'reason'):
+            print 'We failed to reach a server.'
+            print 'Reason: ', e.reason
+        elif hasattr(e, 'code'):
+            print 'The server couldn\'t fulfill the request.'
+            print 'Error code: ', e.code
+    else:
+        # everything is fine
+        pass # do nothing
+
+bitstampTicker_str = get_full_json(baseurl, ticker='bitstampTicker')
+bitstampTicker_obj = json.loads(bitstampTicker_str)
+print bitstampTicker_obj['last']
